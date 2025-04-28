ad883fb6aded679b6d014a64d64b4a8fffdaaaa6
Apotts00 <apotts00@email.cpcc.edu>
2025-04-28 19:54:04 -0400

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const taskRoutes = require('./routes/tasks');  // ✅ <--- NEW
-const User = require('./models/User');
 
 dotenv.config();
 

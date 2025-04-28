0de8621d25ccfebbcf914469357508fa284bcd45
Apotts00 <apotts00@email.cpcc.edu>
2025-04-28 19:52:00 -0400

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,6 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv = require('dotenv');
-const authRoutes = require('./routes/authRoutes');
-const eventRoutes = require('./routes/eventRoutes');
 const taskRoutes = require('./routes/tasks');  // ✅ <--- NEW
 const User = require('./models/User');
 

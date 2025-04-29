0620abeb90f71469ba2e54f833be696ae147200b
Apotts00 <apotts00@email.cpcc.edu>
2025-04-28 21:49:20 -0400

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 const taskRoutes = require("./routes/tasks");
-app.use("/routes/tasks", task);
+app.use("/api/tasks", taskRoutes);
 
 mongoose
   .connect(process.env.MONGO_URI)
